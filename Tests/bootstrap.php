e9ea7264c37c5a5c933a4ebef5ba1766cd8d6e29
marcel-veselka <marcel.veselka@tesena.com>
2015-11-10 00:47:15 +0100

    setting up initial Travis + phpunit files

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,18 +1,4 @@
 <?php
-<?php
+
 require_once __DIR__.'/support/WebDriverAssertions.php';
 require_once __DIR__.'/support/WebDriverDevelop.php';
-
-/*
-
-function loader($class)
-{
-    $file = $class . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-}
-spl_autoload_register('loader');
-
-*/
-
